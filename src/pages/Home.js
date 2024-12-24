084435be1d4cf48e89b7d10ec473d5eb3b0fba26
jen_807 <80chill@naver.com>
2024-12-24 14:12:35 +0900

    add snowflake to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import homebackground from "../img/homebackground.png";
 import { Link } from "react-router-dom";
 import TitleWrap from "../components/TitleWrap";
-// import { textDecorationLine } from "html2canvas/dist/types/css/property-descriptors/text-decoration-line"
+
+// 눈이 떨어지는 애니메이션
+const snowfall = keyframes`
+  0% {
+    transform: translateY(-10px); /* 최상단에서 시작 */
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(100vh); /* 화면 끝까지 내려감 */
+    opacity: 0; /* 사라짐 */
+  }
+`;
 
 const Container = styled.div`
   width: 100%;
@@ -22,14 +33,29 @@ const Container = styled.div`
     z-index: -1;
   }
 `;
+
+// 눈의 개별 조각 스타일
+const Snowflake = styled.div`
+  position: absolute;
+  top: -20px; /* 최상단에서 10px 위에서 시작 */
+  left: ${(props) => props.left}%; /* 랜덤 위치 */
+  width: ${(props) => props.size}px; /* 눈 크기 */
+  height: ${(props) => props.size}px;
+  background-color: white;
+  border-radius: 50%;
+  animation: ${snowfall} ${(props) => props.duration}s linear infinite;
+  animation-delay: ${(props) => props.delay}s; /* 랜덤한 딜레이 */
+  opacity: ${(props) => props.opacity};
+`;
+
 const Title = styled.div`
-  /* margin-top: 100px; */
   font-family: "AHN_L";
   color: white;
   font-size: 62px;
   text-align: center;
   margin-bottom: 75px;
 `;
+
 const Start = styled.div`
   font-family: "AHN_L";
   width: 155px;
@@ -49,11 +75,45 @@ const Start = styled.div`
   }
 `;
 
+const SnowfallContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none; /* 클릭 이벤트 방지 */
+  overflow: hidden; /* 화면 밖으로 나가는 눈 숨김 */
+`;
+
 const Home = () => {
+  // 눈 조각 생성
+  const createSnowflake = () => ({
+    id: Math.random(),
+    left: Math.random() * 100, // 0 ~ 100% 랜덤 위치
+    size: Math.random() * 4 + 2, // 2 ~ 6px 크기
+    duration: Math.random() * 5 + 7, // 7 ~ 12초 애니메이션 지속 시간
+    delay: Math.random() * 5, // 0 ~ 5초 랜덤 딜레이
+    opacity: Math.random() * 0.8 + 0.2, // 0.2 ~ 1 투명도
+  });
+
+  const snowflakes = Array.from({ length: 50 }, () => createSnowflake());
+
   return (
     <>
       <TitleWrap title="Home" />
       <Container>
+        <SnowfallContainer>
+          {snowflakes.map((flake) => (
+            <Snowflake
+              key={flake.id}
+              left={flake.left}
+              size={flake.size}
+              duration={flake.duration}
+              delay={flake.delay}
+              opacity={flake.opacity}
+            />
+          ))}
+        </SnowfallContainer>
         <Title>
           Do you <br /> wanna build
           <br /> a snowman
